bef636321e48941fe9096547f42c279880fd2efd
xhrisgab <xhris.gab@gmail.com>
2026-03-27 19:30:47 -0400

    pruebas navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="bg-icarus-1 flex-auto text-2xl text-icarus-4">
+      <Navbar />
       <h1>Hello World!{7 + 9}</h1>
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+const Navbar = () => {
+  return (
+    <div className="navbar bg-icarus-1 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Link</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="bg-base-100 rounded-t-none p-2">
+                <li>
+                  <a>Link 1</a>
+                </li>
+                <li>
+                  <a>Link 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
