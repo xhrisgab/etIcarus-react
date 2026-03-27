b46f8ed3ca1bd2cd34e3c4f0432ecd49a181bb9c
xhrisgab <xhris.gab@gmail.com>
2026-03-27 19:22:36 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="bg-icarus-1 flex-auto text-2xl text-icarus-4">
+      <h1>Hello World!{7 + 9}</h1>
+    </div>
+  );
+};
+export default App;
